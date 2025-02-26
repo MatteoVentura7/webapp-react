c244e5b9ef9fbe2dd9432ce588a4b7e117a7979b
MatteoVentura <matteo.ventura18@gmail.com>
2025-02-26 17:18:05 +0100

    fix heading

--- src/components/ui/Heading.jsx
+++ src/components/ui/Heading.jsx
@@ -1,7 +1,7 @@
 export default function Heading({ level, children, ...props }) {
   if (level === 1) {
     return (
-      <h1 className="text-4xl font-bold leading-10" {...props}>
+      <h1 className="text-4xl font-bold pb-2" {...props}>
         {children}
       </h1>
     );
@@ -9,7 +9,7 @@ export default function Heading({ level, children, ...props }) {
 
   if (level === 2) {
     return (
-      <h2 className="text-3xl font-semibold leading-10" {...props}>
+      <h2 className="text-3xl font-semibold pb-2" {...props}>
         {children}
       </h2>
     );
@@ -17,7 +17,7 @@ export default function Heading({ level, children, ...props }) {
 
   if (level === 3) {
     return (
-      <h3 className="text-2xl font-semibold leading-10" {...props}>
+      <h3 className="text-2xl font-semibold pb-2" {...props}>
         {children}
       </h3>
     );
@@ -25,7 +25,7 @@ export default function Heading({ level, children, ...props }) {
 
   if (level === 4) {
     return (
-      <h4 className="text-lg font-small leading-10" {...props}>
+      <h4 className="text-lg font-small pb-2" {...props}>
         {children}
       </h4>
     );
@@ -33,7 +33,7 @@ export default function Heading({ level, children, ...props }) {
 
   if (level === 5) {
     return (
-      <h5 className="text-lg leading-10" {...props}>
+      <h5 className="text-lg pb-2" {...props}>
         {children}
       </h5>
     );
@@ -41,7 +41,7 @@ export default function Heading({ level, children, ...props }) {
 
   if (level === 6) {
     return (
-      <h6 className="text-sm leading-5" {...props}>
+      <h6 className="text-sm pb-2 " {...props}>
         {children}
       </h6>
     );
